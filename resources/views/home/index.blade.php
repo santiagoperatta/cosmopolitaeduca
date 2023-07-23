5b67b42e5c45bf56cd55d5f2178e1642248888b9
santiagoperatta <santiagoperattawpp@gmail.com>
2023-07-23 18:56:29 -0300

    agregando timeline

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -48,7 +48,7 @@
 
     <livewire:home-carreras/>
 
-	<div>
+	<div class="mb-10">
 		<h1 class="mb-5 text-center font-bold text-4xl text-blue-900 leading-tight">Preguntas Frecuentes</h1>
 		@foreach ($preguntasFrecuentes as $pregunta)
 			<div class="">
@@ -58,10 +58,53 @@
 			</div>
     	@endforeach
     </div>
+	<div class="mt-16 w-full max-w-5xl mx-auto p-4">
+		<h1 class="text-center font-bold text-4xl text-blue-900 leading-tight">¿Por qué elegir Cosmopolita Educa?</h1>
+		<!-- Centrar la línea de tiempo -->
+		<div class="mx-auto my-12 relative">
+			<!-- Timeline -->
+			<div class="flex flex-col sm:flex-row justify-between items-center">
+				<!-- Primer paso -->
+				<div class="flex flex-col items-center relative">
+					<div class="flex-shrink-0 w-12 h-12 bg-white border-2 rounded-full flex items-center justify-center z-10 mb-4">
+						<span class="text-blue-900 font-bold text-lg">1</span>
+					</div>
+					<img width="48" height="48" src="https://img.icons8.com/fluency/48/overtime.png" alt="overtime"/>
+					<h3 class="my-2 text-lg font-semibold text-blue-900">Flexibilidad de Horarios</h3>
+					<p class="mx-4 mt-2 text-center text-base text-gray-500">Educación a tu ritmo, sin horarios fijos. Flexibilidad para lograr tus metas desde cualquier lugar.</p>
+				</div>
+	
+				<!-- Línea (oculta en dispositivos móviles) -->
+				<div class="h-0.5 w-full bg-gray-200 absolute top-6 z-0 hidden sm:block"></div>
+	
+				<!-- Segundo paso -->
+				<div class="flex flex-col items-center relative mt-4 sm:mt-0">
+					<div class="flex-shrink-0 w-12 h-12 bg-white border-2 rounded-full flex items-center justify-center z-10 mb-4">
+						<span class="text-blue-900 font-bold text-lg">2</span>
+					</div>
+					<img width="48" height="48" src="https://img.icons8.com/fluency/48/update-left-rotation.png" alt="update-left-rotation"/>
+					<h3 class="my-2 text-lg font-semibold text-blue-900">Contenido Actualizado</h3>
+					<p class="mx-4 mt-2 text-center text-base text-gray-500">Contenido actualizado y adaptado a tus necesidades. Avanza en tu carrera con excelencia académica.</p>
+				</div>
+	
+				<!-- Línea (oculta en dispositivos móviles) -->
+				<div class="h-0.5 w-full bg-gray-200 absolute top-6 z-0 hidden sm:block"></div>
+	
+				<!-- Tercer paso -->
+				<div class="flex flex-col items-center relative mt-4 sm:mt-0">
+					<div class="flex-shrink-0 w-12 h-12 bg-white border-2 rounded-full flex items-center justify-center z-10 mb-4">
+						<span class="text-blue-900 font-bold text-lg">3</span>
+					</div>
+					<img width="48" height="48" src="https://img.icons8.com/fluency/48/conference-background-selected.png" alt="conference-background-selected"/>
+					<h3 class="my-2 text-lg font-semibold text-blue-900">Profesionales Capacitados</h3>
+					<p class="mx-4 mt-2 text-center text-base text-gray-500">Equipo de profesionales comprometidos contigo. Transforma tu futuro con nuestra educación de calidad.</p>
+				</div>
+			</div>
+		</div>
+	</div>
 
     <div class="py-12">
-        <div class="">
-            <h1 class="mb-10 text-center font-bold text-4xl text-blue-900 leading-tight">Nuestros Asociados</h1>
+        <div>
             <section class="banks">
                 <div id="icon-ani">
                     <div class="icons-row position-absolute bg-e" style="
