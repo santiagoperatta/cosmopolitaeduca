37286f42bf2b5c325d4d6cc0e0eb59259bb802dc
santiagoperatta <santiagoperattawpp@gmail.com>
2023-09-10 13:13:28 -0300

    corrigiendo configuracion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-	
-    plugins: [forms],
-};
+module.exports = {
+	content: [
+	  "./resources/**/*.blade.php",
+	  './storage/framework/views/*.php',
+	  "./resources/**/*.js",
+	  "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  }
