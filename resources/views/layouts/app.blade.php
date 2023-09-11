81c19a3adef14907fc7166d4e34d1b33c55d518e
santiagoperatta <santiagoperattawpp@gmail.com>
2023-09-11 17:27:40 -0300

    corrigiendo css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-		<link rel="stylesheet" href="{{ asset('dist/styles/custom.css') }}">
-		<link rel="stylesheet" href="{{ asset('dist/styles/whatsapp.css') }}">
+		<link rel="stylesheet" href="{{ asset('dist/custom.css') }}">
+		<link rel="stylesheet" href="{{ asset('dist/whatsapp.css') }}">
 
 
 
