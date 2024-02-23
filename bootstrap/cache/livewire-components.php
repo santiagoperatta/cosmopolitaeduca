818a6f4b809fcc115a86a27ab1f1734b6f746075
santiagoperatta <santiagoperattawpp@gmail.com>
2024-02-23 19:02:15 -0300

    correcciones de estilos

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,16 @@
+<?php return array (
+  'crear-carrera' => 'App\\Http\\Livewire\\CrearCarrera',
+  'editar-carrera' => 'App\\Http\\Livewire\\EditarCarrera',
+  'filtrar-carreras' => 'App\\Http\\Livewire\\FiltrarCarreras',
+  'galeria' => 'App\\Http\\Livewire\\Galeria',
+  'home-carreras' => 'App\\Http\\Livewire\\HomeCarreras',
+  'instituciones' => 'App\\Http\\Livewire\\Instituciones',
+  'mostar-carreras' => 'App\\Http\\Livewire\\MostarCarreras',
+  'mostrar-carrera' => 'App\\Http\\Livewire\\MostrarCarrera',
+  'mostrar-carreras-v' => 'App\\Http\\Livewire\\MostrarCarrerasV',
+  'mostrar-cursos' => 'App\\Http\\Livewire\\MostrarCursos',
+  'mostrar-lista' => 'App\\Http\\Livewire\\MostrarLista',
+  'preguntas' => 'App\\Http\\Livewire\\Preguntas',
+  'solicitar-carrera' => 'App\\Http\\Livewire\\SolicitarCarrera',
+  'timeline' => 'App\\Http\\Livewire\\Timeline',
+);
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
 	<div class="sectionUno">
 		<div class="fondoinicio py-20 overflow-hidden lg:py-24">
-			<img src="images/formas/shape-08.svg" alt="Shape Bg" class="hidden md:block absolute inset-0 w-full h-full object-cover z-0"/>
 			<div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
 				<div class="p-16 relative">
 					<h2 class="text-center text-4xl leading-8 font-extrabold tracking-tight text-gray-700 sm:text-6xl">Cosmopolita Educa<br>Educación a Distancia</h2>
@@ -17,21 +16,6 @@
 	<livewire:home-carreras/>
 
 	<div class="sectionTres">
-		<svg class="absolute top-0 left-0 w-full h-full">
-			<!-- Círculos rellenos -->
-			<circle cx="20%" cy="20%" r="8" fill="#FFDD0F" class="hidden md:block decorative-circle"/>
-			<circle cx="80%" cy="80%" r="6" fill="#FFDD0F" class="hidden md:block decorative-circle"/>
-			<!-- Círculos como trazos -->
-			<circle cx="40%" cy="50%" r="1" fill="none" stroke="#FFDD0F" stroke-width="1" class="hidden md:block decorative-circle"/>
-			<circle cx="70%" cy="30%" r="1" fill="none" stroke="#FFDD0F" stroke-width="1" class="hidden md:block decorative-circle"/>
-			<!-- Ondas como trazos -->
-			<circle cx="10%" cy="90%" r="10" fill="none" stroke="#FFDD0F" stroke-width="1" opacity="0.5" class="hidden md:block decorative-circle"/>
-			<circle cx="60%" cy="70%" r="8" fill="none" stroke="#FFDD0F" stroke-width="1" opacity="0.5" class="hidden md:block decorative-circle"/>
-			<!-- Puntitos como círculos pequeños -->
-			<circle cx="30%" cy="70%" r="1" fill="#FFDD0F" class="decorative-circle"/>
-			<circle cx="60%" cy="50%" r="1" fill="#FFDD0F" class="decorative-circle"/>
-		</svg>
-		<img src="images/formas/shape-15.svg" alt="Shape Bg" class="absolute inset-0 w-full h-full object-cover z-10"/>
 		<div class="hidden md:block w-full p-20 mb-20 fondobanner" style="background-repeat: no-repeat; position: relative; z-index: 5;">
 			<div class="max-w-lg mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
 				<div class="flex">
